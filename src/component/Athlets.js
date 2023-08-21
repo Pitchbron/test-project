058afbfeaa77133bc929b1c3c6d8bbef15d6e9f9
Pitchbron <pitchayut879@gmail.com>
2023-08-21 21:57:58 +0700

    finish touch-slide

--- src/component/Athlets.js
+++ src/component/Athlets.js
@@ -1,69 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 import Topic from "./Topic";
-import currentSlide from "./carousel/currentSlide";
 
 const Athlets = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
+  });
+
   return (
     <div style={{ position: "relative" }}>
       <div className="title title-position1">ATHLETS</div>
-      <div className="slideshow-container">
-        <div className="container1 mySlides displayBlock">
-          <div className="group-right top-topic1">
-            <Topic
-              topicNumber="01"
-              topicName="CONNECTION"
-              underlineColor="purple"
-            >
-              Connect with coaches directly, you can ping coaches to view
-              profile.
-            </Topic>
-          </div>
-        </div>
 
-        <div className="container2 mySlides">
-          <div className="group-right top-topic2">
-            <Topic
-              topicNumber="02"
-              topicName="COLLABORATION"
-              underlineColor="purple"
-            >
-              Work with other student athletes to increase visability. When you
-              share and like other players videos it will increase your
-              visability as a player. This is the team work aspect to Surface 1.
-            </Topic>
+      <div className="navigation-wrapper">
+        <div ref={sliderRef} className="keen-slider">
+          <div className="keen-slider__slide container1">
+            <div className="group-right top-topic1">
+              <Topic
+                topicNumber="01"
+                topicName="CONNECTION"
+                underlineColor="purple"
+              >
+                Connect with coaches directly, you can ping coaches to view
+                profile.
+              </Topic>
+            </div>
           </div>
-        </div>
 
-        <div className="container3-1 mySlides">
-          <div className="group-right top-topic3">
-            <Topic topicNumber="03" topicName="GROWTH" underlineColor="white">
-              Resources and tools for you to get better as a student Athelte.
-              Access to training classes, tutor sessions, etc.
-            </Topic>
+          <div className="keen-slider__slide container2">
+            <div className="group-right top-topic2">
+              <Topic
+                topicNumber="02"
+                topicName="COLLABORATION"
+                underlineColor="purple"
+              >
+                Work with other student athletes to increase visability. When
+                you share and like other players videos it will increase your
+                visability as a player. This is the team work aspect to Surface
+                1.
+              </Topic>
+            </div>
+          </div>
+
+          <div className="keen-slider__slide container3-1">
+            <div className="group-right top-topic3">
+              <Topic topicNumber="03" topicName="GROWTH" underlineColor="white">
+                Resources and tools for you to get better as a student Athelte.
+                Access to training classes, tutor sessions, etc.
+              </Topic>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="carousel-nav">
-        <button
-          className="carousel-indicator active"
-          onClick={() => {
-            currentSlide(1, "mySlides", "carousel-indicator");
-          }}
-        ></button>
-        <button
-          className="carousel-indicator"
-          onClick={() => {
-            currentSlide(2, "mySlides", "carousel-indicator");
-          }}
-        ></button>
-        <button
-          className="carousel-indicator"
-          onClick={() => {
-            currentSlide(3, "mySlides", "carousel-indicator");
-          }}
-        ></button>
-      </div>
+      {loaded && instanceRef.current && (
+        <div
+          className="dots"
+          style={{ position: "absolute", zIndex: "10", left: "50%" }}
+        >
+          {[
+            ...Array(instanceRef.current.track.details.slides.length).keys(),
+          ].map((idx) => {
+            return (
+              <button
+                key={idx}
+                onClick={() => {
+                  instanceRef.current?.moveToIdx(idx);
+                }}
+                className={"dot" + (currentSlide === idx ? " active" : "")}
+              ></button>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/component/Players.js
+++ src/component/Players.js
@@ -1,67 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 import Topic from "./Topic";
-import currentSlide from "./carousel/currentSlide";
 
 const Players = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
+  });
+
   return (
     <div style={{ position: "relative" }}>
       <div className="title title-position2">PLAYERS</div>
-      <div className="slideshow-container">
-        <div className="container1-2 mySlides-2 displayBlock">
-          <div className="group-left top-topic4">
-            <Topic
-              topicNumber="01"
-              topicName="CONNECTION"
-              underlineColor="purple"
-            >
-              Connect with talented athlete directly, you can watch their skills
-              through video showreels directly from Surface 1.
-            </Topic>
-          </div>
-        </div>
 
-        <div className="container2-2 mySlides-2">
-          <div className="group-left top-topic5">
-            <Topic
-              topicNumber="02"
-              topicName="COLLABORATION"
-              underlineColor="purple"
-            >
-              Work with recruiter to increase your chances of finding talented
-              athlete.
-            </Topic>
+      <div className="navigation-wrapper">
+        <div ref={sliderRef} className="keen-slider">
+          <div className="keen-slider__slide container1-2">
+            <div className="group-left top-topic4">
+              <Topic
+                topicNumber="01"
+                topicName="CONNECTION"
+                underlineColor="purple"
+              >
+                Connect with talented athlete directly, you can watch their
+                skills through video showreels directly from Surface 1.
+              </Topic>
+            </div>
           </div>
-        </div>
 
-        <div className="container3-2 mySlides-2">
-          <div className="group-left top-topic6">
-            <Topic topicNumber="03" topicName="GROWTH" underlineColor="white">
-              Save your time, recruit proper athlets for your team.
-            </Topic>
+          <div className="keen-slider__slide container2-2">
+            <div className="group-left top-topic5">
+              <Topic
+                topicNumber="02"
+                topicName="COLLABORATION"
+                underlineColor="purple"
+              >
+                Work with recruiter to increase your chances of finding talented
+                athlete.
+              </Topic>
+            </div>
+          </div>
+
+          <div className="keen-slider__slide container3-2">
+            <div className="group-left top-topic6">
+              <Topic topicNumber="03" topicName="GROWTH" underlineColor="white">
+                Save your time, recruit proper athlets for your team.
+              </Topic>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="carousel-nav">
-        <button
-          className="carousel-indicator-2 active"
-          onClick={() => {
-            currentSlide(1, "mySlides-2", "carousel-indicator-2");
-          }}
-        ></button>
-        <button
-          className="carousel-indicator-2"
-          onClick={() => {
-            currentSlide(2, "mySlides-2", "carousel-indicator-2");
-          }}
-        ></button>
-        <button
-          className="carousel-indicator-2"
-          onClick={() => {
-            currentSlide(3, "mySlides-2", "carousel-indicator-2");
-          }}
-        ></button>
-      </div>
+      {loaded && instanceRef.current && (
+        <div
+          className="dots"
+          style={{ position: "absolute", zIndex: "10", left: "50%" }}
+        >
+          {[
+            ...Array(instanceRef.current.track.details.slides.length).keys(),
+          ].map((idx) => {
+            return (
+              <button
+                key={idx}
+                onClick={() => {
+                  instanceRef.current?.moveToIdx(idx);
+                }}
+                className={"dot" + (currentSlide === idx ? " active" : "")}
+              ></button>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
